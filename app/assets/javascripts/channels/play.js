f581ace26d248c5d14bd68adf8def7d83f782dd6
nrfn <59220732+nrfn@users.noreply.github.com>
2020-04-11 16:52:32 +0900

    js Refactoring

--- app/assets/javascripts/channels/play.js
+++ app/assets/javascripts/channels/play.js
@@ -1,5 +1,6 @@
 $(function() {
 
+  //音を扱う変数を作成する--------------------
   var c_4 = new Tone.Synth().toMaster();
   var c_s_4 = new Tone.Synth().toMaster();
   var d_4 = new Tone.Synth().toMaster();
@@ -32,6 +33,7 @@ $(function() {
   var d_s_6 = new Tone.Synth().toMaster();
   var e_6 = new Tone.Synth().toMaster();
 
+  //鍵盤のスイッチ用変数---------------------
   var c4 = 0;
   var cs4 = 0;
   var d4 = 0;
@@ -64,8 +66,8 @@ $(function() {
   var ds6 = 0;
   var e6 = 0;
 
+  //キーボード機能のスイッチ-----------
   var i = 0;
-  
   $("#switch").change(function() {
     if(i == 0){
       i = 1;
@@ -76,79 +78,81 @@ $(function() {
     }
   });
 
+  //キーボードの処理（キーダウン）-------------------------------------------------------------------------------------------------------------------------------
 	document.onkeydown = 
     function keydown(e) {
       if (i == 1){
-        if( c4==0 && e.keyCode=='81' ){ c4 =1;document.getElementById( "c_4" ).style.backgroundColor= "pink"; c_4.triggerAttackRelease('C4','4n');}
-        if( cs4==0 && e.keyCode=='50' ){ cs4 =1;document.getElementById( "c_s_4" ).style.backgroundColor= "pink"; c_s_4.triggerAttackRelease('C#4','4n');}
-        if( d4==0 && e.keyCode=='87' ) { d4 =1;document.getElementById( "d_4" ).style.backgroundColor= "pink"; d_4.triggerAttackRelease('D4','4n');}
-        if( ds4==0 && e.keyCode=='51' ) { ds4 =1;document.getElementById( "d_s_4" ).style.backgroundColor= "pink"; d_s_4.triggerAttackRelease('D#4','4n');}
-        if( e4==0 && e.keyCode=='69' ) { e4 =1;document.getElementById( "e_4" ).style.backgroundColor= "pink"; e_4.triggerAttackRelease('E4','4n');}
-        if( f4==0 && e.keyCode=='82' ) { f4 =1;document.getElementById( "f_4" ).style.backgroundColor= "pink"; f_4.triggerAttackRelease('F4','4n');}
-        if( fs4==0 && e.keyCode=='53' ) { fs4 =1;document.getElementById( "f_s_4" ).style.backgroundColor= "pink"; f_s_4.triggerAttackRelease('F#4','4n');}
-        if( g4==0 && e.keyCode=='84' ) { g4 =1;document.getElementById( "g_4" ).style.backgroundColor= "pink"; g_4.triggerAttackRelease('G4','4n');}
-        if( gs4==0 && e.keyCode=='54' ) { gs4 =1;document.getElementById( "g_s_4" ).style.backgroundColor= "pink"; g_s_4.triggerAttackRelease('G#4','4n');}
-        if( a4==0 && e.keyCode=='89' ) { a4 =1;document.getElementById( "a_4" ).style.backgroundColor= "pink"; a_4.triggerAttackRelease('A4','4n');}
-        if( as4==0 && e.keyCode=='55' ) { as4 =1;document.getElementById( "a_s_4" ).style.backgroundColor= "pink"; a_s_4.triggerAttackRelease('A#4','4n');}
-        if( b4==0 && e.keyCode=='85' ) { b4 =1;document.getElementById( "b_4" ).style.backgroundColor= "pink"; b_4.triggerAttackRelease('B4','4n');}
+        if( c4==0 && e.keyCode=='81' ){ c4=1; document.getElementById("c_4").style.backgroundColor="pink"; c_4.triggerAttackRelease('C4','4n'); }
+        if( cs4==0 && e.keyCode=='50' ){ cs4=1; document.getElementById("c_s_4").style.backgroundColor="pink"; c_s_4.triggerAttackRelease('C#4','4n'); }
+        if( d4==0 && e.keyCode=='87' ){ d4=1; document.getElementById("d_4").style.backgroundColor="pink"; d_4.triggerAttackRelease('D4','4n'); }
+        if( ds4==0 && e.keyCode=='51' ){ ds4=1; document.getElementById("d_s_4").style.backgroundColor="pink"; d_s_4.triggerAttackRelease('D#4','4n'); }
+        if( e4==0 && e.keyCode=='69' ){ e4=1; document.getElementById("e_4").style.backgroundColor="pink"; e_4.triggerAttackRelease('E4','4n'); }
+        if( f4==0 && e.keyCode=='82' ){ f4=1; document.getElementById("f_4").style.backgroundColor="pink"; f_4.triggerAttackRelease('F4','4n'); }
+        if( fs4==0 && e.keyCode=='53' ){ fs4=1; document.getElementById("f_s_4").style.backgroundColor="pink"; f_s_4.triggerAttackRelease('F#4','4n'); }
+        if( g4==0 && e.keyCode=='84' ){ g4=1; document.getElementById("g_4").style.backgroundColor="pink"; g_4.triggerAttackRelease('G4','4n'); }
+        if( gs4==0 && e.keyCode=='54' ){ gs4=1; document.getElementById("g_s_4").style.backgroundColor="pink"; g_s_4.triggerAttackRelease('G#4','4n'); }
+        if( a4==0 && e.keyCode=='89' ){ a4=1; document.getElementById("a_4").style.backgroundColor="pink"; a_4.triggerAttackRelease('A4','4n'); }
+        if( as4==0 && e.keyCode=='55' ){ as4=1; document.getElementById("a_s_4").style.backgroundColor="pink"; a_s_4.triggerAttackRelease('A#4','4n'); }
+        if( b4==0 && e.keyCode=='85' ){ b4=1; document.getElementById("b_4").style.backgroundColor="pink"; b_4.triggerAttackRelease('B4','4n'); }
 
-        if( c5==0 && e.keyCode=='73' ){ c5 =1;document.getElementById( "c_5" ).style.backgroundColor= "pink"; c_5.triggerAttackRelease('C5','4n');}
-        if( cs5==0 && e.keyCode=='57' ){ cs5 =1;document.getElementById( "c_s_5" ).style.backgroundColor= "pink"; c_s_5.triggerAttackRelease('C#5','4n');}
-        if( d5==0 && e.keyCode=='79' ) { d5 =1;document.getElementById( "d_5" ).style.backgroundColor= "pink"; d_5.triggerAttackRelease('D5','4n');}
-        if( ds5==0 && e.keyCode=='48' ) { ds5 =1;document.getElementById( "d_s_5" ).style.backgroundColor= "pink"; d_s_5.triggerAttackRelease('D#5','4n');}
-        if( e5==0 && e.keyCode=='80' ) { e5 =1;document.getElementById( "e_5" ).style.backgroundColor= "pink"; e_5.triggerAttackRelease('E5','4n');}
-        if( f5==0 && e.keyCode=='90' ) { f5 =1;document.getElementById( "f_5" ).style.backgroundColor= "pink"; f_5.triggerAttackRelease('F5','4n');}
-        if( fs5==0 && e.keyCode=='83' ) { fs5 =1;document.getElementById( "f_s_5" ).style.backgroundColor= "pink"; f_s_5.triggerAttackRelease('F#5','4n');}
-        if( g5==0 && e.keyCode=='88' ) { g5 =1;document.getElementById( "g_5" ).style.backgroundColor= "pink"; g_5.triggerAttackRelease('G5','4n');}
-        if( gs5==0 && e.keyCode=='68' ) { gs5 =1;document.getElementById( "g_s_5" ).style.backgroundColor= "pink"; g_s_5.triggerAttackRelease('G#5','4n');}
-        if( a5==0 && e.keyCode=='67' ) { a5 =1;document.getElementById( "a_5" ).style.backgroundColor= "pink"; a_5.triggerAttackRelease('A5','4n');}
-        if( as5==0 && e.keyCode=='70' ) { as5 =1;document.getElementById( "a_s_5" ).style.backgroundColor= "pink"; a_s_5.triggerAttackRelease('A#5','4n');}
-        if( b5==0 && e.keyCode=='86' ) { b5 =1;document.getElementById( "b_5" ).style.backgroundColor= "pink"; b_5.triggerAttackRelease('B5','4n');}
+        if( c5==0 && e.keyCode=='73' ){ c5=1; document.getElementById("c_5").style.backgroundColor="pink"; c_5.triggerAttackRelease('C5','4n'); }
+        if( cs5==0 && e.keyCode=='57' ){ cs5=1; document.getElementById("c_s_5").style.backgroundColor="pink"; c_s_5.triggerAttackRelease('C#5','4n'); }
+        if( d5==0 && e.keyCode=='79' ){ d5=1; document.getElementById("d_5").style.backgroundColor="pink"; d_5.triggerAttackRelease('D5','4n'); }
+        if( ds5==0 && e.keyCode=='48' ){ ds5=1; document.getElementById("d_s_5").style.backgroundColor="pink"; d_s_5.triggerAttackRelease('D#5','4n'); }
+        if( e5==0 && e.keyCode=='80' ){ e5=1; document.getElementById("e_5").style.backgroundColor="pink"; e_5.triggerAttackRelease('E5','4n'); }
+        if( f5==0 && e.keyCode=='90' ){ f5=1; document.getElementById("f_5").style.backgroundColor="pink"; f_5.triggerAttackRelease('F5','4n'); }
+        if( fs5==0 && e.keyCode=='83' ){ fs5=1; document.getElementById("f_s_5").style.backgroundColor="pink"; f_s_5.triggerAttackRelease('F#5','4n'); }
+        if( g5==0 && e.keyCode=='88' ){ g5=1; document.getElementById("g_5").style.backgroundColor="pink"; g_5.triggerAttackRelease('G5','4n'); }
+        if( gs5==0 && e.keyCode=='68' ){ gs5=1; document.getElementById("g_s_5").style.backgroundColor="pink"; g_s_5.triggerAttackRelease('G#5','4n'); }
+        if( a5==0 && e.keyCode=='67' ){ a5=1; document.getElementById("a_5").style.backgroundColor="pink"; a_5.triggerAttackRelease('A5','4n'); }
+        if( as5==0 && e.keyCode=='70' ){ as5=1; document.getElementById("a_s_5").style.backgroundColor="pink"; a_s_5.triggerAttackRelease('A#5','4n'); }
+        if( b5==0 && e.keyCode=='86' ){ b5=1; document.getElementById("b_5").style.backgroundColor="pink"; b_5.triggerAttackRelease('B5','4n'); }
 
-        if( c6==0 && e.keyCode=='66' ){ c6 =1;document.getElementById( "c_6" ).style.backgroundColor= "pink"; c_6.triggerAttackRelease('C6','4n');}
-        if( cs6==0 && e.keyCode=='72' ){ cs6 =1;document.getElementById( "c_s_6" ).style.backgroundColor= "pink"; c_s_6.triggerAttackRelease('C#6','4n');}
-        if( d6==0 && e.keyCode=='78' ) { d6 =1;document.getElementById( "d_6" ).style.backgroundColor= "pink"; d_6.triggerAttackRelease('D6','4n');}
-        if( ds6==0 && e.keyCode=='74' ) { ds6 =1;document.getElementById( "d_s_6" ).style.backgroundColor= "pink"; d_s_6.triggerAttackRelease('D#6','4n');}
-        if( e6==0 && e.keyCode=='77' ) { e6 =1;document.getElementById( "e_6" ).style.backgroundColor= "pink"; e_6.triggerAttackRelease('E6','4n');}
+        if( c6==0 && e.keyCode=='66' ){ c6=1; document.getElementById("c_6").style.backgroundColor="pink"; c_6.triggerAttackRelease('C6','4n'); }
+        if( cs6==0 && e.keyCode=='72' ){ cs6=1; document.getElementById("c_s_6").style.backgroundColor="pink"; c_s_6.triggerAttackRelease('C#6','4n'); }
+        if( d6==0 && e.keyCode=='78' ){ d6=1; document.getElementById("d_6").style.backgroundColor="pink"; d_6.triggerAttackRelease('D6','4n'); }
+        if( ds6==0 && e.keyCode=='74' ){ ds6=1; document.getElementById("d_s_6").style.backgroundColor="pink"; d_s_6.triggerAttackRelease('D#6','4n'); }
+        if( e6==0 && e.keyCode=='77' ){ e6=1; document.getElementById("e_6").style.backgroundColor="pink"; e_6.triggerAttackRelease('E6','4n'); }
       }
     };
 
+    //キーボードの処理（キーアップ）------------------------------------------------------------------------
 	document.onkeyup = 
 	  function keyup(e) {
-      if(e.keyCode== '81'){ c4 = 0; document.getElementById("c_4").style.backgroundColor="white"; }
-      if(e.keyCode== '50'){ cs4 = 0; document.getElementById("c_s_4").style.backgroundColor="black"; }
-      if (e.keyCode== '87'){ d4 = 0; document.getElementById("d_4").style.backgroundColor="white"; }
-      if (e.keyCode== '51'){ ds4 = 0; document.getElementById("d_s_4").style.backgroundColor="black"; }
-      if (e.keyCode== '69'){ e4 = 0; document.getElementById("e_4").style.backgroundColor="white"; }
-      if (e.keyCode== '82'){ f4 = 0; document.getElementById("f_4").style.backgroundColor="white"; }
-      if (e.keyCode== '53'){ fs4 = 0; document.getElementById("f_s_4").style.backgroundColor="black"; }
-      if (e.keyCode== '84'){ g4 = 0; document.getElementById("g_4").style.backgroundColor="white"; }
-      if (e.keyCode== '54'){ gs4 = 0; document.getElementById("g_s_4").style.backgroundColor="black"; }
-      if (e.keyCode== '89'){ a4 = 0; document.getElementById("a_4").style.backgroundColor="white"; }
-      if (e.keyCode== '55'){ as4 = 0; document.getElementById("a_s_4").style.backgroundColor="black"; }
-      if (e.keyCode== '85'){ b4 = 0; document.getElementById("b_4").style.backgroundColor="white"; }
+      if( e.keyCode=='81' ){ c4=0; document.getElementById("c_4").style.backgroundColor="white"; }
+      if( e.keyCode=='50' ){ cs4=0; document.getElementById("c_s_4").style.backgroundColor="black"; }
+      if ( e.keyCode=='87' ){ d4=0; document.getElementById("d_4").style.backgroundColor="white"; }
+      if ( e.keyCode=='51' ){ ds4=0; document.getElementById("d_s_4").style.backgroundColor="black"; }
+      if ( e.keyCode=='69' ){ e4=0; document.getElementById("e_4").style.backgroundColor="white"; }
+      if ( e.keyCode=='82' ){ f4=0; document.getElementById("f_4").style.backgroundColor="white"; }
+      if ( e.keyCode=='53' ){ fs4=0; document.getElementById("f_s_4").style.backgroundColor="black"; }
+      if ( e.keyCode=='84' ){ g4=0; document.getElementById("g_4").style.backgroundColor="white"; }
+      if ( e.keyCode=='54' ){ gs4=0; document.getElementById("g_s_4").style.backgroundColor="black"; }
+      if ( e.keyCode=='89' ){ a4=0; document.getElementById("a_4").style.backgroundColor="white"; }
+      if ( e.keyCode=='55' ){ as4=0; document.getElementById("a_s_4").style.backgroundColor="black"; }
+      if ( e.keyCode=='85' ){ b4=0; document.getElementById("b_4").style.backgroundColor="white"; }
 
-      if(e.keyCode== '73'){ c5 = 0; document.getElementById("c_5").style.backgroundColor="white"; }
-      if(e.keyCode== '57'){ cs5 = 0; document.getElementById("c_s_5").style.backgroundColor="black"; }
-      if(e.keyCode== '79'){ d5 = 0; document.getElementById("d_5").style.backgroundColor="white"; }
-      if (e.keyCode== '48'){ ds5 = 0; document.getElementById("d_s_5").style.backgroundColor="black"; }
-      if (e.keyCode== '80'){ e5 = 0; document.getElementById("e_5").style.backgroundColor="white"; }
-      if (e.keyCode== '90'){ f5 = 0; document.getElementById("f_5").style.backgroundColor="white"; }
-      if (e.keyCode== '83'){ fs5 = 0; document.getElementById("f_s_5").style.backgroundColor="black"; }
-      if (e.keyCode== '88'){ g5 = 0; document.getElementById("g_5").style.backgroundColor="white"; }
-      if (e.keyCode== '68'){ gs5 = 0; document.getElementById("g_s_5").style.backgroundColor="black"; }
-      if (e.keyCode== '67'){ a5 = 0; document.getElementById("a_5").style.backgroundColor="white"; }
-      if (e.keyCode== '70'){ as5 = 0; document.getElementById("a_s_5").style.backgroundColor="black"; }
-      if (e.keyCode== '86'){ b5 = 0; document.getElementById("b_5").style.backgroundColor="white"; }
+      if( e.keyCode=='73' ){ c5=0; document.getElementById("c_5").style.backgroundColor="white"; }
+      if( e.keyCode=='57' ){ cs5=0; document.getElementById("c_s_5").style.backgroundColor="black"; }
+      if( e.keyCode=='79' ){ d5=0; document.getElementById("d_5").style.backgroundColor="white"; }
+      if( e.keyCode=='48' ){ ds5=0; document.getElementById("d_s_5").style.backgroundColor="black"; }
+      if( e.keyCode=='80' ){ e5=0; document.getElementById("e_5").style.backgroundColor="white"; }
+      if( e.keyCode=='90' ){ f5=0; document.getElementById("f_5").style.backgroundColor="white"; }
+      if( e.keyCode=='83' ){ fs5=0; document.getElementById("f_s_5").style.backgroundColor="black"; }
+      if( e.keyCode=='88' ){ g5=0; document.getElementById("g_5").style.backgroundColor="white"; }
+      if( e.keyCode=='68' ){ gs5=0; document.getElementById("g_s_5").style.backgroundColor="black"; }
+      if( e.keyCode=='67' ){ a5=0; document.getElementById("a_5").style.backgroundColor="white"; }
+      if( e.keyCode=='70' ){ as5=0; document.getElementById("a_s_5").style.backgroundColor="black"; }
+      if( e.keyCode=='86' ){ b5=0; document.getElementById("b_5").style.backgroundColor="white"; }
 
-      if(e.keyCode== '66'){ c6 = 0; document.getElementById("c_6").style.backgroundColor="white"; }
-      if(e.keyCode== '72'){ cs6 = 0; document.getElementById("c_s_6").style.backgroundColor="black"; }
-      if (e.keyCode== '78'){ d6 = 0; document.getElementById("d_6").style.backgroundColor="white"; }
-      if (e.keyCode== '74'){ ds6 = 0; document.getElementById("d_s_6").style.backgroundColor="black"; }
-      if (e.keyCode== '77'){ e6 = 0; document.getElementById("e_6").style.backgroundColor="white"; }
+      if( e.keyCode=='66' ){ c6=0; document.getElementById("c_6").style.backgroundColor="white"; }
+      if( e.keyCode=='72' ){ cs6=0; document.getElementById("c_s_6").style.backgroundColor="black"; }
+      if( e.keyCode=='78' ){ d6=0; document.getElementById("d_6").style.backgroundColor="white"; }
+      if( e.keyCode=='74' ){ ds6=0; document.getElementById("d_s_6").style.backgroundColor="black"; }
+      if( e.keyCode=='77' ){ e6=0; document.getElementById("e_6").style.backgroundColor="white"; }
     };
 
-  //-----------------------------------------------------------------------------------------------------------------
+  //-------------------------------------------------------------------------------------------------
   var synth = new Tone.Synth().toMaster();  //シンセの音を作成
   var sound_code = {                        //テキストと音のコード
     "ド": "C5",
@@ -159,7 +163,6 @@ $(function() {
     "ラ": "A5",
     "シ": "B5",
 
-//----------------
     "c4": "C4",
     "d4": "D4",
     "e4": "E4",
@@ -176,7 +179,6 @@ $(function() {
     "A4": "A#4",
     "B4": "B#4",
 
-//-----------------
     "c5": "C5",
     "d5": "D5",
     "e5": "E5",
@@ -193,7 +195,6 @@ $(function() {
     "A5": "A#5",
     "B5": "B#5",
 
-//------------------
     "c6": "C6",
     "d6": "D6",
     "e6": "E6",
@@ -205,12 +206,12 @@ $(function() {
   }
 
 //---------------------------------------------------------------------------------------------------------------------
-  function addMelody(time, note) {                  //音を出す処理 noteに音のコード,timeにはボタンが押されてからの時間が入っている
+  function addMelody(time, note) {                  //音を出す処理 note(音のコード),time(時間)
       synth.triggerAttackRelease(note, '8n', time);
       console.log(note);
   }
 
-//-----------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------
   document.getElementById("play").onclick = function() {
       var melody_score = [];                        //実際に鳴らす音を入れる配列
       var score = document.getElementById("score"); //score(ノード要素)取得する
